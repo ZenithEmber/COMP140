f813c0e3389b041e8dc9a3f2561d21bd64d30029
ZenithEmber <ph230156@falmouth.ac.uk>
2020-03-02 19:23:46 +0000

    Created level for player, added AI gameObject, Added player movement and mouseLook scripts to look around.

---
+++ Badminton/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    /*
+     * Starting variables
+     * MouseSensitivity set so it is the same for everyone at the start and we can use it to make the movements independent to the framerate when used.
+     * Declaring the playerbody we need to add so the program knows what we are rotating. 
+     * 
+     */
+
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lock the cursor so it stays in the game screen.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //with the use of the charactercontroller component we can use the input system to get the movement of the mouse ingame without having to declare a lot of variables that calculate the axises
+        //multiply the movement of the mouse with the sensitivity and Time.deltatime so it is not sensitive to framerate and the movement feels smooth.
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Rotating on the Y axis. And clamping the rotation so we don't break our neck while playing.
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //Rotation on the x axis.
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ Badminton/Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller; //Variable to initialise the controller we use for the movement.
+
+    public float speed = 12f; //float for the speed at which the controller moves
+    public float gravity = -9.81f;
+    public float jumpHeight = 5f;
+    private bool sprinting = false;
+
+    public float CastDist = 1f; //float value that dictates the distance of the raycast
+
+    Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         *inistialize the X and Z axis movement to keep track of which way we move. 
+         * Can populate the values with the charactercontroller functions of input.GetAxis and the according direction axis
+         */
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Calculate the movement multiply the transform so the movement is always in according to the way the character is looking, rather than the actual position in Unity
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //Move the character in the direction calculated above and adding the speed of movement and making it relative to the Time so the framerate does not influence it
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && Physics.Raycast(transform.position, Vector3.down, CastDist))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            sprinting = true;
+            if (sprinting == true)
+            {
+                speed = 12f;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            sprinting = false;
+            if (sprinting = false)
+            {
+                speed = 8f;
+            }
+        }
+    }
+}
